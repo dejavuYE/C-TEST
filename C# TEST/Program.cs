e97346f2e3f901b4f981474e9ad016721e5a6e27
Dejavu <leon_ye@mail.ru>
2024-09-12 22:12:59 +0300

    Добавьте файлы проекта.

---
+++ C# TEST/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+for(int i = 1; i < 10; i++)
+{
+    for(int j = 1; j < 10; j++)
+    {
+        Console.Write((i * j) + " ");
+    }
+    Console.WriteLine();
+}
